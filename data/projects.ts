427d865cfd7d71a9ea4f8d58fe65d3b11c191c23
mosabbir-ui <hello.uiverse.bd@gmail.com>
2025-09-28 12:12:07 +0600

    add a new project

--- data/projects.ts
+++ data/projects.ts
@@ -74,4 +74,94 @@ export const projects: Project[] = [
     image: '/images/project-12.png',
     preview: "https://shipmateai.org/"
   },
+  {
+    title: 'JVAI FoodAR',
+    description: 'A restaurant ordering and management app with real-time updates.',
+    image: '/images/project-13.jpg',
+    preview: "https://crocoshark.com/"
+  },
+  {
+    title: 'E Clinic',
+    description: 'A restaurant ordering and management app with real-time updates.',
+    image: '/images/project-14.jpg',
+    preview: "https://e-clinic.ai/"
+  },
+  {
+    title: 'RentX Rewards',
+    description: 'A restaurant ordering and management app with real-time updates.',
+    image: '/images/project-15.jpg',
+    preview: "https://rentxrewards.com/"
+  },
+  {
+    title: 'Chez Tati',
+    description: 'A restaurant ordering and management app with real-time updates.',
+    image: '/images/project-16.jpg',
+    preview: "https://www.cheztati.ca/en"
+  },
+  {
+    title: 'Redakcja Naukowa',
+    description: 'A restaurant ordering and management app with real-time updates.',
+    image: '/images/project-17.jpg',
+    preview: "https://redakcjanaukowa.com/"
+  },
+  {
+    title: 'Earkick',
+    description: 'A restaurant ordering and management app with real-time updates.',
+    image: '/images/project-18.jpg',
+    preview: "https://chat.earkick.com/"
+  },
+  {
+    title: 'AI Chatbot',
+    description: 'A restaurant ordering and management app with real-time updates.',
+    image: '/images/project-19.jpg',
+    preview: "https://chat-website-demo.aidbase.ai/"
+  },
+  {
+    title: 'Trend Assist',
+    description: 'A restaurant ordering and management app with real-time updates.',
+    image: '/images/project-20.jpg',
+    preview: "https://app.trendassist.ai/"
+  },
+  {
+    title: 'Hever Care',
+    description: 'A restaurant ordering and management app with real-time updates.',
+    image: '/images/project-21.jpg',
+    preview: "https://www.hevercare.co.uk/"
+  },
+  {
+    title: 'Just in Time Hero',
+    description: 'A restaurant ordering and management app with real-time updates.',
+    image: '/images/project-22.jpg',
+    preview: "https://justintimehero.com/"
+  },
+  {
+    title: 'science gig',
+    description: 'A restaurant ordering and management app with real-time updates.',
+    image: '/images/project-24.jpg',
+    preview: "https://science-gig-website.vercel.app/"
+  },
+  {
+    title: 'Redakcja Naukowa',
+    description: 'A restaurant ordering and management app with real-time updates.',
+    image: '/images/project-25.jpg',
+    preview: "https://justyna-website.vercel.app/"
+  },
+  {
+    title: 'Find Work',
+    description: 'A restaurant ordering and management app with real-time updates.',
+    image: '/images/project-26.jpg',
+    preview: "https://play.google.com/store/apps/details?id=com.findings.job"
+  },
+  {
+    title: 'Accura Meet',
+    description: 'A restaurant ordering and management app with real-time updates.',
+    image: '/images/project-27.jpg',
+    preview: "https://play.google.com/store/apps/details?id=com.sagigroup.accurameet&pli=1"
+  },
+  {
+    title: 'United',
+    description: 'A restaurant ordering and management app with real-time updates.',
+    image: '/images/project-28.jpg',
+    preview: "https://play.google.com/store/apps/details?id=com.united.bycadz&hl=en&pli=1"
+  },
 ];
